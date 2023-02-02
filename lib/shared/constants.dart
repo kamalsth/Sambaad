9ea55c5632c3c5affef8a839cf857359d743f534
Kamal Shrestha <kamalshrestha583@gmail.com>
2023-02-02 16:45:16 +0545

    Initial Requirements setting for project sambaad1

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 class Constants{
-  static String appId="1:1084881602704:web:ca435ade8b7fb39380b015";
-  static String apiKey="AIzaSyCWBnqgQYvx2gnkqrWE-dTXrIkpfkvdB9g";
-  static String messagingSenderId="1084881602704";
-  static String projectId="sambaad-a40f5";
+  static String appId="1:379702995571:web:0be44ab986ddad88961806";
+  static String apiKey="AIzaSyDJE8W2WTgYn4OnqPSA1ZqGODus7BR8-eM";
+  static String messagingSenderId="379702995571";
+  static String projectId="sambaad-kamal";
   //final primaryColor=const Color(0xFFee7b64);
   //final primaryColor=const Color(0xff075E54);
   final primaryColor=Color.fromARGB(255, 50, 155, 253);
